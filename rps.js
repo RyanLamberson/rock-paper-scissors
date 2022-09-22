3f3243041a319506e6949dff95f9cd3d41afd387
Ryan Lamberson <lambersonryan348@gmail.com>
2022-09-22 12:30:47 -0400

    refactor to buttons

--- rps.js
+++ rps.js
@@ -1,116 +1,107 @@
+let playerScore = 0;
+let computerScore =0;
+let totalScore =0;
+let roundCount =0;
+let pChoice = document.querySelector("#pChoice");
+let cChoice = document.querySelector("#cChoice");
+
+console.log(roundCount);
 function getComputerChoice ()
 {
     temp = Math.floor(Math.random()*10);
     choice = temp % 3;
     switch (choice) {
         case 0:
-            console.log("Computer chooses Rock");
+            cChoice.textContent = "Computer chooses Rock";
             return 0;
         case 1:
-            console.log("Computer chooses Paper");
+            cChoice.textContent="Computer chooses Paper";
             return 1;
         case 2:
-            console.log("Computer chooses Scissors");
+            cChoice.textContent="Computer chooses Scissors";
             return 2;
         
     }
 }
-function getPlayerChoice()
-{
-    inputText = prompt("Rock, paper, scissors, SHOOT!", "???")
-    if (inputText.toUpperCase().localeCompare("ROCK")==0)
-    {
-        return 0;
-    }
-    else if (inputText.toUpperCase().localeCompare("SCISSORS")==0)
-    {
-        return 2;
-    }
-    else if (inputText.toUpperCase().localeCompare("PAPER")==0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 4;
-    }
-}
-
 function playRound(playerChoice)
 {
     result = -2;
-    while (playerChoice==4)
-    {
-        playerChoice=getPlayerChoice();
-    }
     computerChoice=getComputerChoice();
-    if (playerChoice==computerChoice)
+    btn = playerChoice.textContent;
+    console.log(btn);
+    pcResult = -1;
+    roundCount = roundCount+1;
+    console.log(roundCount);
+    label = document.querySelector("#Results");
+    
+    if (btn.toUpperCase().localeCompare("ROCK")==0)
     {
-        result =0;
-    } 
-    else if (playerChoice==0 && computerChoice==1)
-    {
-        result= -1;
+        pcResult =0;
+        pChoice.textContent = "Player chooses Rock"
+        
     }
-    else if (playerChoice==0 && computerChoice==2)
+    else if (btn.toUpperCase().localeCompare("PAPER")==0)
     {
-        result= 1;
-    } 
-    else if (playerChoice==1 && computerChoice==0)
-    {
-        result= 1;
+        pcResult= 1;
+        pChoice.textContent = "Player chooses Paper"
+
+    
     }
-    else if (playerChoice==1 && computerChoice==2)
+    else if (btn.toUpperCase().localeCompare("SCISSORS")==0)
     {
-        result= -1;
-    }
-    else if (playerChoice==2 && computerChoice==0)
-    {
-        result= -1;
-    }
-    else if (playerChoice==2 && computerChoice==1)
-    {
-        result= 1;
+        pcResult= 2;
+        pChoice.textContent = "Player chooses Scissors"
+
     }
 
-    if (result==-1)
+    if (pcResult == computerChoice)
     {
-        console.log("Computer wins this round.");
-    }    
-    else if (result==1)
-    {
-        console.log("Player wins this round.");
+        result= 0;
     }
-    else if (result==0)
+    else if ((pcResult==0 && computerChoice ==2)||(pcResult==1 && computerChoice==0)||
+            (pcResult==2 && computerChoice==1))
     {
-        console.log("Tie round.")
+        result = 1;
+        playerScore++;
+        console.log(playerScore);
     }
-    else
+    else if ((pcResult==0 && computerChoice ==1)||(pcResult==1 && computerChoice==2)||
+        (pcResult==2 && computerChoice==0))
     {
-        console.log("Error: " + playerChoice + " : "+computerChoice);
-    }
-    return result;
-}
-function game()
-{
-    score =0;
-    for (let i = 0; i < 5; i++)
-    {
-        score = score + playRound(getPlayerChoice());
-    }
-    if (score > 0)
-    {
-        console.log("Player wins!");
-    }
-    else if (score==0)
-    {
-        console.log("Tie!");
-    }
-    else
-    {
-        console.log("Computer wins!");
+        result = -1;
+        computerScore--;
     }
 
-    return "Final Score: " + score;
+    gameEnd = "ERROR";
+
+    totalScore = playerScore + computerScore;
+    label.textContent = "Player Score: " + playerScore + " :: Computer Score: " + computerScore;
+    if (roundCount == 5)
+    {
+        if (playerScore > computerScore*-1)
+        {
+            gameEnd = "Player Wins!";
+        }
+        else if (playerScore < computerScore*-1)
+        {
+            gameEnd = "Computer Wins!";
+        }
+        else
+        {
+            gameEnd = "Tie Game!";
+        }
+        label.textContent = label.textContent + " :: " + gameEnd;
+        playerScore = 0;
+        computerScore =0;
+        totalScore =0;
+        roundCount =0;
+    }
+    
 }
-console.log(game());
+
+buttons = document.getElementsByTagName('button');
+console.log(buttons);
+let buttonArray = Array.from(buttons);
+console.log(buttonArray);
+buttonArray.forEach((button)=>{button.addEventListener('click', 
+    function() {playRound(button);})});
